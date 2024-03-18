d4a16099a91d2b8aa322b05a8ccdc0c9aa6c29b9
AtsuAtsu <58623243+AtsuAtsu0120@users.noreply.github.com>
2024-03-18 15:11:57 +0900

    feat: ScriptableBranch

---
+++ Assets/ConversationGraph/Runtime/Core/Interfaces/IScriptableBranch.cs
@@ -0,0 +1,9 @@
+﻿namespace ConversationGraph.Runtime.Core.Interfaces
+{
+    public interface IScriptableBranch
+    {
+        public string Guid { get; set; }
+        public int BranchCount { get; set; }
+        public int OnArrival();
+    }
+}
