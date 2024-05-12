784622c4d9c778ee3cf1e09a42877ff36ebb05d0
AtsuAtsu <58623243+AtsuAtsu0120@users.noreply.github.com>
2024-05-12 19:19:00 +0900

    feat: Add History Template

--- Assets/ConversationGraph/Editor/Foundation/EditorExtensions.cs
+++ Assets/ConversationGraph/Editor/Foundation/EditorExtensions.cs
@@ -6,6 +6,7 @@ namespace ConversationGraph.Editor.Foundation
     public static class EditorExtensions
     {
         private const string templateGuid = "5eea233c2f50d5242a3c4fa0fb36e30b";
+        private const string templateWithHistoryGuid = "b44299410e19e064394d5103f8855aeb";
         
         [MenuItem("GameObject/ConversationGraph/Template")]
         public static void CreateTemplate()
@@ -13,6 +14,14 @@ namespace ConversationGraph.Editor.Foundation
             var prefabAssetPath = AssetDatabase.GUIDToAssetPath(templateGuid);
             var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabAssetPath);
             Object.Instantiate(prefab);
-        }        
+        }
+        
+        [MenuItem("GameObject/ConversationGraph/Template with History")]
+        public static void CreateTemplateWithHistory()
+        {
+            var prefabAssetPath = AssetDatabase.GUIDToAssetPath(templateWithHistoryGuid);
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(prefabAssetPath);
+            Object.Instantiate(prefab);
+        }
     }
 }
